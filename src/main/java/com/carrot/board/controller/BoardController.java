bddfebff5d08a9349d70e9518477e523c64e5615
mmirlea <jeunmr95@gmail.com>
2023-05-31 12:08:45 +0900

    pageHandler, SearchCondition 추가

--- src/main/java/com/carrot/board/controller/BoardController.java
+++ src/main/java/com/carrot/board/controller/BoardController.java
@@ -1,8 +1,5 @@
 package com.carrot.board.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,29 +16,16 @@ public class BoardController {
 
 	@GetMapping("/read")
 	public String read(Integer b_num, Integer page, Integer pageSize, Model m) {
+		try {
+			BoardDTO boardDTO = service.read(0);
+			m.addAttribute(boardDTO);
+			m.addAttribute("page", page);
+			m.addAttribute("pageSize", pageSize);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
-		System.out.println("test!!");
-
-//		try {
-//			BoardDTO boardDTO = service.read(b_num);
-//
-//			m.addAttribute("boardDTO", boardDTO);
-//
-//			m.addAttribute("page", page);
-//			m.addAttribute("pageSize", pageSize);
-//
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-
-		return "board";
-	}
-
-	// 로그인 여부 확인
-	private boolean loginCheck(HttpServletRequest request) {
-		System.out.println("test22");
-		HttpSession session = request.getSession();
-		return session.getAttribute("id") != null;
+		return "boardMain";
 	}
 
 }
