b45a17a81707773610545e991cc70534b6a7aaa8
dkssudghejr <dkssudghejr@naver.com>
2023-06-05 12:01:17 +0900

    샛별

--- src/main/java/com/carrot/board/dao/CommentsDAOImpl.java
+++ src/main/java/com/carrot/board/dao/CommentsDAOImpl.java
@@ -11,20 +11,23 @@ import org.springframework.stereotype.Repository;
 import com.carrot.board.domain.CommentsDTO;
 
 @Repository
-public class CommentsDAOImpl {
+public class CommentsDAOImpl implements CommentsDAO {
 	@Autowired
 	SqlSession session;
 	
 	String namespace = "com.carrot.board.dao.CommentsMapper.";
 	
+	@Override
 	public int deleteAll(Integer c_pnum) throws Exception{
 		return session.delete(namespace+"deleteAll", c_pnum);
 	}
 	
+	@Override
 	public int count(Integer c_pnum) throws Exception{
 		return session.selectOne(namespace+"count", c_pnum);
 	}
 	
+	@Override
 	public int delete(Integer c_num, String c_email) throws Exception{
 		Map map = new HashMap();
 		map.put("c_num", c_num);
@@ -32,18 +35,22 @@ public class CommentsDAOImpl {
 		return session.delete(namespace+"delete", map);
 	}
 	
+	@Override
 	public int insert(CommentsDTO dto) throws Exception{
 		return session.insert(namespace+"insert", dto);
 	}
 	
+	@Override
 	public List<CommentsDTO> selectAll(Integer c_pnum) throws Exception{
 		return session.selectList(namespace+"selectAll", c_pnum);
 	}
 	
+	@Override
 	public CommentsDTO select(Integer c_num) throws Exception{
 		return session.selectOne(namespace+"select", c_num);
 	}
 	
+	@Override
 	public int update(CommentsDTO dto) throws Exception{
 		return session.update(namespace+"update", dto);
 	}
