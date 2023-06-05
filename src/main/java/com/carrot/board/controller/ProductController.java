76008c9425265c09a5dc81ae79ecd57da1d71675
jbokchi <antwerp789@gmail.com>
2023-06-05 10:57:12 +0900

    del commit

--- src/main/java/com/carrot/board/controller/ProductController.java
+++ src/main/java/com/carrot/board/controller/ProductController.java
@@ -3,13 +3,17 @@ package com.carrot.board.controller;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.carrot.board.domain.BoardDTO;
 import com.carrot.board.domain.PageHandler;
 import com.carrot.board.domain.PageHandlerP;
 import com.carrot.board.domain.ProductDTO;
@@ -53,12 +57,15 @@ public class ProductController {
 		
 	//중고물풀 게시글 읽기
 	@GetMapping("/read")
-	public String read(Integer p_num, Model m) {
+	public String read(Integer p_num, Model m, HttpSession session) {
+		String login_email = (String) session.getAttribute("m_email");
 		try {
 			ProductDTO productDTO = service.read(p_num);
 			
 			m.addAttribute(productDTO);
 			m.addAttribute("menu", "product");
+			
+			session.setAttribute("login_email", login_email);
 		}catch(Exception e) {
 			e.printStackTrace();
 			m.addAttribute("msg", "READ_ERR");
@@ -75,4 +82,54 @@ public class ProductController {
 		return "JunggoDetail";
 	}
 	
+	@PostMapping("/remove")
+	public String remove(Integer p_num, Integer page, Integer pageSize, RedirectAttributes rattr, Model m,
+			HttpSession session) {
+		String p_email = (String) session.getAttribute("m_email");
+
+		try {
+			m.addAttribute("page", page);
+			m.addAttribute("pageSize", pageSize);
+
+			int rowCnt = service.remove(p_num, p_email);
+			
+			
+
+			if (rowCnt != 1) {
+				throw new Exception("remove error");
+			}
+
+			rattr.addFlashAttribute("msg", "DEL_OK");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			rattr.addFlashAttribute("msg", "DEL_ERR");
+		}
+
+		return "redirect:/carrot/junggoMain";
+	}
+
+	@PostMapping("/modify")
+	public String modify(ProductDTO productDTO, RedirectAttributes rattr, Model m, HttpSession session) {
+		String p_email = (String) session.getAttribute("m_email");
+		productDTO.setP_email(p_email);
+		try {
+			if (service.modify(productDTO) != 1)
+				throw new Exception("Modify failed");
+
+			rattr.addFlashAttribute("msg", "MOD_OK");
+
+			return "redirect:/carrot/list";
+
+		} catch (Exception e) {
+			e.printStackTrace();
+
+			//m.addAttribute("mode", "new"); 
+			m.addAttribute("menu", "board");
+			m.addAttribute("productDTO", productDTO); 
+			m.addAttribute("msg", "MOD_ERR"); 
+
+			return "junggoDetail"; 
+		}
+	}
 }
